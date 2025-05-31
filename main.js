8bf8041be5f8da05109d282e413f3c8b41fcab1f
Isabelli Lucena <isabelli.llucena@gmail.com>
2025-05-31 16:02:03 -0300

    início da funçãi de removeItem() para remover os itens da lista

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const items = []
 
 function addItem() {
-    const itemNome = document.querySelector("#item").value
+    const itemName = document.querySelector("#item").value
     
     const item = {
-        name: itemNome,
+        name: itemName,
         checked: false
     }
 
@@ -31,11 +31,21 @@ function showItemsList() {
                 <label for="item-${index}">${item.name}</label>
             </div>
 
-            <button>
+            <button onclick="removeItem('${item.name}')">
                 <img src="./assets/trash-icon.svg" alt="trash icon">
             </button>
 
             </div>
         `
     })
+}
+
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+
+    if (itemIndex !== -1) {
+        items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
 }
